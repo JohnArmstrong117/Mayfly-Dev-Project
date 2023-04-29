32b9f3d3e8a59e006b296edea941357316257f70
John Armstrong <johnarmstrong117@gmail.com>
2023-04-28 21:42:21 -0700

    Adding data members

--- Mayfly-Dev-Solution/Mayfly-LogicEngine/User.cs
+++ Mayfly-Dev-Solution/Mayfly-LogicEngine/User.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Mayfly_LogicEngine
 {
-    internal class Class1
+    /// <summary>
+    /// Abstract class base for student and teacher users.
+    /// </summary>
+    internal abstract class User
     {
+        protected string name;
+
+        protected string password;
+
+        protected string userID;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        /// <param name="newName">string name of user.</param>
+        /// <param name="newPass">string password.</param>
+        /// <param name="newUID">string userID.</param>
+        public User(string newName, string newPass, string newUID)
+        {
+            this.name = newName;
+            this.password = newPass;
+            this.userID = newUID;
+        }
     }
 }
