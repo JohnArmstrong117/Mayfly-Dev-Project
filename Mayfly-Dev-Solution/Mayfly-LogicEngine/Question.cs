cb0a4aa0f4cbfca22389685ddaf81ba69dd8ceac
John Armstrong <johnarmstrong117@gmail.com>
2023-04-28 20:38:43 -0700

    Adding Data Members to Question

--- Mayfly-Dev-Solution/Mayfly-LogicEngine/Question.cs
+++ Mayfly-Dev-Solution/Mayfly-LogicEngine/Question.cs
@@ -1,7 +1,37 @@
 ﻿namespace Mayfly_LogicEngine
 {
+    /// <summary>
+    /// Class designed to store question data.
+    /// </summary>
     public class Question
     {
+        private string questionText;
+        private string[] answersText = new string[4];
+        private int correctIndex;
 
+        /// <summary>
+        /// Gets the correct index for the questions answer.
+        /// </summary>
+        public int CorrectIndex
+        {
+            get { return this.correctIndex; }
+        }
+
+        /// <summary>
+        /// Gets an array of strings containing the answers to the question.
+        /// </summary>
+        public string[] AnswerText
+        {
+            get { return this.answersText; }
+        }
+
+        /// <summary>
+        /// Gets or Sets the questions text.
+        /// </summary>
+        public string QuestionText
+        {
+            get { return this.questionText; }
+            set { this.questionText = value; }
+        }
     }
 }
