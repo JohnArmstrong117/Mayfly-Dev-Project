e42e94b427b460e22753c383a923e63f3e004ffc
John Armstrong <johnarmstrong117@gmail.com>
2023-04-28 21:32:07 -0700

    Adding Data Members

--- Mayfly-Dev-Solution/Mayfly-LogicEngine/SchoolClass.cs
+++ Mayfly-Dev-Solution/Mayfly-LogicEngine/SchoolClass.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Mayfly_LogicEngine
 {
+    /// <summary>
+    /// class designed to store data for SchoolClasses.
+    /// </summary>
     internal class SchoolClass
     {
+        private List<Quiz> quizList;
+        private string name;
+        private List<User> students;
+        private User teacher;
+
+        /// <summary>
+        /// Gets or Sets this Class' active teacher.
+        /// </summary>
+        public User Teacher
+        {
+            get { return this.teacher; }
+            set { this.teacher = value; }
+        }
+
+        /// <summary>
+        /// Gets the list of enrolled students.
+        /// </summary>
+        public List<User> Students
+        {
+            get { return this.students; }
+        }
+
+        /// <summary>
+        /// Gets or Sets the Name of the class.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        /// <summary>
+        /// Gets a list of quizzes contained in this SchoolClass.
+        /// </summary>
+        public List<Quiz> QuizList
+        {
+            get { return this.quizList; }
+        }
     }
 }
