8119120fa4fc6e643c1e86a7fee9e9cc7fc09d55
John Armstrong <johnarmstrong117@gmail.com>
2023-04-28 21:56:18 -0700

    Adding Data members and constructor

--- Mayfly-Dev-Solution/Mayfly-LogicEngine/Teacher.cs
+++ Mayfly-Dev-Solution/Mayfly-LogicEngine/Teacher.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Mayfly_LogicEngine
 {
-    internal class Teacher
+    /// <summary>
+    /// Class designed to hold data for teachers.
+    /// </summary>
+    internal class Teacher : User
     {
+        private List<SchoolClass> schoolClasses;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Teacher"/> class.
+        /// </summary>
+        /// <param name="newName"></param>
+        /// <param name="newPass"></param>
+        /// <param name="newUID"></param>
+        public Teacher(string newName, string newPass, string newUID) 
+            : base(newName, newPass, newUID)
+        {
+            // TODO: Implement Constructor.
+        }
+
+        /// <summary>
+        /// Gets a list of this teachers classes.
+        /// </summary>
+        public List<SchoolClass> SchoolClasses
+        {
+            get { return this.SchoolClasses; }
+        }
     }
 }
