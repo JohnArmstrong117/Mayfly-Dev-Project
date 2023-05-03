9b8c3f4676c74cb145d60e5cebfec68098e9e528
John Armstrong <johnarmstrong117@gmail.com>
2023-05-02 17:58:05 -0700

    Adding LogIn user Control

--- Mayfly-Dev-Solution/Mayfly-UI/Form1.Designer.cs
+++ Mayfly-Dev-Solution/Mayfly-UI/Form1.Designer.cs
@@ -28,18 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            logInPage1 = new LogInPage();
             SuspendLayout();
+            // 
+            // logInPage1
+            // 
+            logInPage1.BackColor = SystemColors.ControlDark;
+            logInPage1.Location = new Point(-2, -1);
+            logInPage1.Name = "logInPage1";
+            logInPage1.Size = new Size(803, 451);
+            logInPage1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(logInPage1);
             Name = "Form1";
             Text = "Mayfly Quiz";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private LogInPage logInPage1;
     }
 }
---
+++ Mayfly-Dev-Solution/Mayfly-UI/LogInPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mayfly_UI
+{
+    public partial class LogInPage : UserControl
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == string.Empty || this.textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter both a username and password.");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
