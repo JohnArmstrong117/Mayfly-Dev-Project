0703bc5132f9e92f276a10cdb0b5ddb49b343b85
John Armstrong <johnarmstrong117@gmail.com>
2023-05-03 22:46:05 -0700

    Adding most login functionality

--- Mayfly-Dev-Solution/Mayfly-UI/LogInPage.cs
+++ Mayfly-Dev-Solution/Mayfly-UI/LogInPage.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Mayfly_LogicEngine;
 
 namespace Mayfly_UI
 {
     public partial class LogInPage : UserControl
     {
+        private LogInManager logInManager;
         public LogInPage()
         {
             InitializeComponent();
+            logInManager = new LogInManager();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,6 +30,36 @@ namespace Mayfly_UI
             if (this.textBox1.Text == string.Empty || this.textBox2.Text == string.Empty)
             {
                 MessageBox.Show("Please enter both a username and password.");
+            }
+            string username = this.textBox1.Text;
+            string password = this.textBox2.Text;
+            if (this.logInManager.UserExists(username))
+            {
+                User foundUser = this.logInManager.getUserByUsername(username);
+                if (foundUser != null)
+                {
+                    if (password == foundUser.Password)
+                    {
+                        if (foundUser is Teacher)
+                        {
+                            // TODO: LOGIn to teacher Ui with user
+                        }
+                        else if (foundUser is Student)
+                        {
+                            // TODO: LOGIN to student ui with user
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect username or password.");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No matching User Found.");
+                return;
             }
         }
 
