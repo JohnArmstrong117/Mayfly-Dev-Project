283cb68e42da02041ab3f20cdae7dae5f01d4b68
John Armstrong <johnarmstrong117@gmail.com>
2023-05-04 16:53:19 -0700

    Initial commit

---
+++ Mayfly-Dev-Solution/Mayfly-UI/TeacherViewClasses.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Mayfly_UI
+{
+    partial class TeacherViewClasses
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(192, 255, 255);
+            label1.Font = new Font("Stencil", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(29, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Classes";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(199, 27);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(376, 320);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 192);
+            button1.Location = new Point(47, 79);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 64);
+            button1.TabIndex = 2;
+            button1.Text = "Create New Class";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 192, 192);
+            button2.Location = new Point(47, 162);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 61);
+            button2.TabIndex = 3;
+            button2.Text = "Return to Home";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // ViewAccountPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(label1);
+            Name = "ViewAccountPage";
+            Size = new Size(605, 382);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+    }
+}
