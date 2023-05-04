f96aa8d47dffe0ac9465e04daf9fa50c6582ece0
John Armstrong <johnarmstrong117@gmail.com>
2023-05-04 00:30:52 -0700

    Adding Application Manager

--- Mayfly-Dev-Solution/Mayfly-UI/LogInPage.cs
+++ Mayfly-Dev-Solution/Mayfly-UI/LogInPage.cs
@@ -14,10 +14,16 @@ namespace Mayfly_UI
     public partial class LogInPage : UserControl
     {
         private LogInManager logInManager;
+        private Form1? parForm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogInPage"/> class.
+        /// </summary>
         public LogInPage()
         {
             InitializeComponent();
-            logInManager = new LogInManager();
+            this.logInManager = new LogInManager();
+            this.parForm = this.ParentForm as Form1;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -40,14 +46,16 @@ namespace Mayfly_UI
                 {
                     if (password == foundUser.Password)
                     {
-                        if (foundUser is Teacher)
+                        if (foundUser is Teacher && this.parForm != null)
                         {
                             // TODO: LOGIn to teacher Ui with user
+                            this.parForm.AppManager.ActiveUser = foundUser;
                             MessageBox.Show("Success Teacher Logged In.");
                         }
-                        else if (foundUser is Student)
+                        else if (foundUser is Student && this.parForm != null)
                         {
                             // TODO: LOGIN to student ui with user
+                            this.parForm.AppManager.ActiveUser = foundUser;
                             MessageBox.Show("Success Student Logged In.");
                         }
                     }
