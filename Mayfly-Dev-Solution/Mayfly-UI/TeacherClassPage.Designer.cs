bcae38bb852b5aacdcb62fca80172a886c7f541c
John Armstrong <johnarmstrong117@gmail.com>
2023-05-04 18:43:11 -0700

    Impleneting functionality initial

--- Mayfly-Dev-Solution/Mayfly-UI/TeacherClassPage.Designer.cs
+++ Mayfly-Dev-Solution/Mayfly-UI/TeacherClassPage.Designer.cs
@@ -28,18 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            classNameLabel = new Label();
+            returnToHomeButton = new Button();
+            label1 = new Label();
+            newNameBox = new TextBox();
+            createQuizButton = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             SuspendLayout();
+            // 
+            // classNameLabel
+            // 
+            classNameLabel.AutoSize = true;
+            classNameLabel.BackColor = Color.FromArgb(192, 255, 255);
+            classNameLabel.Font = new Font("Stencil", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            classNameLabel.Location = new Point(24, 25);
+            classNameLabel.Name = "classNameLabel";
+            classNameLabel.Size = new Size(121, 35);
+            classNameLabel.TabIndex = 0;
+            classNameLabel.Text = "label1";
+            // 
+            // returnToHomeButton
+            // 
+            returnToHomeButton.BackColor = Color.FromArgb(255, 192, 192);
+            returnToHomeButton.Location = new Point(38, 77);
+            returnToHomeButton.Name = "returnToHomeButton";
+            returnToHomeButton.Size = new Size(94, 29);
+            returnToHomeButton.TabIndex = 1;
+            returnToHomeButton.Text = "Home";
+            returnToHomeButton.UseVisualStyleBackColor = false;
+            returnToHomeButton.Click += returnToHomeButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 40);
+            label1.TabIndex = 2;
+            label1.Text = "To Create a quiz enter the\r\nnew quiz name bellow:";
+            // 
+            // newNameBox
+            // 
+            newNameBox.Location = new Point(24, 175);
+            newNameBox.Name = "newNameBox";
+            newNameBox.Size = new Size(179, 27);
+            newNameBox.TabIndex = 3;
+            newNameBox.TextChanged += newNameBox_TextChanged;
+            // 
+            // createQuizButton
+            // 
+            createQuizButton.BackColor = Color.FromArgb(192, 255, 192);
+            createQuizButton.Location = new Point(38, 219);
+            createQuizButton.Name = "createQuizButton";
+            createQuizButton.Size = new Size(94, 29);
+            createQuizButton.TabIndex = 4;
+            createQuizButton.Text = "Create Quiz";
+            createQuizButton.UseVisualStyleBackColor = false;
+            createQuizButton.Click += createQuizButton_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(235, 24);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(276, 314);
+            flowLayoutPanel1.TabIndex = 5;
             // 
             // TeacherClassPage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(createQuizButton);
+            Controls.Add(newNameBox);
+            Controls.Add(label1);
+            Controls.Add(returnToHomeButton);
+            Controls.Add(classNameLabel);
             Name = "TeacherClassPage";
             Size = new Size(549, 367);
             Load += TeacherClassPage_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label classNameLabel;
+        private Button returnToHomeButton;
+        private Label label1;
+        private TextBox newNameBox;
+        private Button createQuizButton;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Mayfly-Dev-Solution/Mayfly-UI/TeacherClassPage.cs
+++ Mayfly-Dev-Solution/Mayfly-UI/TeacherClassPage.cs
@@ -18,15 +18,61 @@ namespace Mayfly_UI
         {
             InitializeComponent();
             this.className = className;
+            this.classNameLabel.Text = className;
         }
 
         public string ClassName
         {
-            get { return this.className;  }
+            get { return this.className; }
             set { this.className = value; }
         }
 
         private void TeacherClassPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void returnToHomeButton_Click(object sender, EventArgs e)
+        {
+            Form1? parForm = this.ParentForm as Form1;
+            if (parForm != null)
+            {
+                TeacherHomePage thp = new TeacherHomePage();
+                parForm.SwitchUserControl(thp);
+            }
+        }
+
+        private void newNameBox_TextChanged(object sender, EventArgs e)
+        {
+            if (this.newNameBox.Text == string.Empty)
+            {
+                this.createQuizButton.Enabled = false;
+            }
+            else
+            {
+                this.createQuizButton.Enabled = true;
+            }
+        }
+
+        private void createQuizButton_Click(object sender, EventArgs e)
+        {
+            Form1? parForm = this.ParentForm as Form1;
+            if (parForm != null)
+            {
+                // TODO add quiz to class
+                Button quizButton = new Button();
+                quizButton.Text = this.newNameBox.Text;
+                quizButton.Size = new Size(94, 60);
+
+                this.flowLayoutPanel1.Controls.Add(quizButton);
+                quizButton.Click += new EventHandler(this.QuizButton_Click);
+            }
+
+            this.newNameBox.Text = string.Empty;
+            this.createQuizButton.Enabled = false;
+        }
+
+        private void QuizButton_Click(object sender, EventArgs e)
         {
 
         }
