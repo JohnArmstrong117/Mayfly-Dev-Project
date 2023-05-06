a9a7a024344373d342147e3e97543bf9e5187961
John Armstrong <johnarmstrong117@gmail.com>
2023-05-05 21:13:47 -0700

    Initial implementation

---
+++ Mayfly-Dev-Solution/Mayfly-UI/TeacherQuestionView.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Mayfly_UI
+{
+    partial class TeacherQuestionView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label2 = new Label();
+            backButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(192, 255, 255);
+            label1.Font = new Font("Stencil", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(25, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 35);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(28, 71);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(515, 270);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 128, 128);
+            label2.Location = new Point(152, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(242, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Click A Question below to delete it.";
+            // 
+            // backButton
+            // 
+            backButton.BackColor = Color.FromArgb(255, 128, 128);
+            backButton.Location = new Point(409, 13);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(134, 42);
+            backButton.TabIndex = 3;
+            backButton.Text = "Return to Quiz";
+            backButton.UseVisualStyleBackColor = false;
+            backButton.Click += backButton_Click;
+            // 
+            // TeacherQuestionView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            Controls.Add(backButton);
+            Controls.Add(label2);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(label1);
+            Name = "TeacherQuestionView";
+            Size = new Size(566, 360);
+            Load += TeacherQuestionView_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label2;
+        private Button backButton;
+    }
+}
---
+++ Mayfly-Dev-Solution/Mayfly-UI/TeacherQuestionView.cs
@@ -0,0 +1,71 @@
+﻿using Mayfly_LogicEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mayfly_UI
+{
+    public partial class TeacherQuestionView : UserControl
+    {
+
+        private Quiz activeQuiz;
+
+        public TeacherQuestionView(Quiz q)
+        {
+            InitializeComponent();
+            this.activeQuiz = q;
+            this.label1.Text = q.QuizName;
+            this.LoadQuestionsToFlow();
+        }
+
+        private void LoadQuestionsToFlow()
+        {
+            foreach (Question question in this.activeQuiz.Questions)
+            {
+                Button quizButton = new Button();
+                quizButton.Text = question.QuestionText;
+                quizButton.Size = new Size(200, 50);
+                quizButton.BackColor = Color.OrangeRed;
+                quizButton.Click += new EventHandler(this.OnQuizButtonPress);
+                this.flowLayoutPanel1.Controls.Add(quizButton);
+            }
+        }
+
+        private void OnQuizButtonPress(object sender, EventArgs e)
+        {
+            Button clicked = (Button)sender;
+            string qt = clicked.Text;
+            List<Question> questions = this.activeQuiz.Questions;
+            foreach (Question q in questions)
+            {
+                if (q.QuestionText == qt)
+                {
+                    this.activeQuiz.Questions.Remove(q);
+                    this.flowLayoutPanel1.Controls.Remove(clicked);
+                    break;
+                }
+            }
+        }
+
+        private void TeacherQuestionView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            Form1? parForm = this.ParentForm as Form1;
+            if (parForm != null)
+            {
+                TeacherQuizPage tqp = new TeacherQuizPage(this.activeQuiz.QuizName);
+                parForm.SwitchUserControl(tqp);
+            }
+        }
+    }
+}
